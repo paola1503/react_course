8533e198c3b7010625eafe49a3aeef38e21acf58
Paola Petri Ortiz <78741678+paola1503@users.noreply.github.com>
2021-05-04 13:22:40 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,25 @@ app.use(cors());
 
 let students=[];
 students.push({"name":"Joe",mark:5},{"name":"Anna",mark:7});
+
+//Ex: Joe
+app.delete("/students/:name",(req,res)=>{
+
+  //Ex: object that has Joe as its name
+  let selected=students.filter(function(element){
+    return element.name==req.params.name;
+  })[0];
+
+  let index=students.indexOf(selected);
+  students.splice(index,1);
+  res.status(204).send();
+})
+
+app.post("/students",(req,res)=>{
+  students.push(req.body);
+  res.status(201).send();
+})
+
 app.get("/students",(req, res)=>{
     res.send(students);
 })
